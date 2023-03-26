c15bb339719e0ba405d4e3f4e42f86849f877d23
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-03-26 22:58:43 +0200

    Add: planet controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import prisma from './prisma.service';
 
-@Controller()
-export class AppController {
+@Controller('planets')
+export class PlanetController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async findAll() {
+    return prisma.planet.findMany();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { PlanetController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [PlanetController],
   providers: [AppService],
 })
 export class AppModule {}
